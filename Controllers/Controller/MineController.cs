ca004e640adbb558969082e9eedc39ce03174283
yesq <wangyesq@gmail.com>
2018-04-18 15:02:13 +0800

    控制器

---
+++ Controllers/Controller/MineController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProposalManageSystem.Controllers
+{
+    public class MineController : Controller
+    {
+        // GET: Mine
+        public ActionResult Index()
+        {
+            return View("mine");
+        }
+    }
+}
---
+++ Controllers/Controller/PersonalController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProposalManageSystem.Controllers
+{
+    public class PersonalController : Controller
+    {
+        // GET: Personal
+        public ActionResult Index()
+        {
+            return View("personal");
+        }
+    }
+}
